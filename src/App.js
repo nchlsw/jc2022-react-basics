73a789619cdc4b118ac9113153cad46c418c2114
nchlsw <nichowuu@gmail.com>
2022-03-02 10:04:48 +0700

    add .map

--- src/App.js
+++ src/App.js
@@ -6,14 +6,48 @@ import ClassComponent from "./components/ClassComponent/ClassComponent";
 import ContentCard from "./components/ContentCard/ContentCard";
 
 //react can only return 1 element, to return more than 1 element use react fragment <> </>
+
+const data = [
+  {
+    username: "Mark",
+    location: "BSD",
+    numberOfLikes: 120,
+    caption: "Halo",
+  },
+  {
+    username: "Seto",
+    location: "Jakarta",
+    numberOfLikes: 3,
+    caption: "Hello World",
+  },
+  {
+    username: "Bill",
+    location: "Puncak",
+    numberOfLikes: 314,
+    caption: "Halo Dunia",
+  },
+];
 function App() {
+  const renderContentlist = () => {
+    return data.map((val) => {
+      return (
+        <ContentCard
+          username={val.username}
+          location={val.location}
+          numberOfLikes={val.numberOfLikes}
+          caption={val.caption}
+        />
+      );
+    });
+  };
+
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <div className="container">
         <div className="row">
-          <div className="col-6 offset-3">
-            <ContentCard />
+          <div className="col-12 col-md-6 offset-md-3">
+            {renderContentlist()}
           </div>
         </div>
       </div>
--- src/components/ClassComponent/ClassComponent.jsx
+++ src/components/ClassComponent/ClassComponent.jsx
@@ -1,21 +1,21 @@
-import  {
-    Card,
-    CardBody,
-    CardTitle,
-    CardSubtitle,
-    CardFooter }
-    from 'reactstrap'
-    import newyork from './newyork.jpeg'
- 
+import React from "react";
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardFooter,
+} from "reactstrap";
+import newyork from "./newyork.jpeg";
 
 class ClassComponent extends React.Component {
-render () {
+  render() {
     return (
-    <div>
-    <h1>Ini Class Component</h1>
-    </div>
-    )
-}
+      <div>
+        <h1>Ini Class Component</h1>
+      </div>
+    );
+  }
 }
 
-export default ClassComponent
+export default ClassComponent;
